8bcee7e308e7545fc6d2a927259fd48ac0ac6c9f
NoName0946 <168062386+NoName0946@users.noreply.github.com>
2024-04-27 06:38:39 +0000

    first rect component

--- continued/src/App.js
+++ continued/src/App.js
@@ -1,8 +1,14 @@
+//THis is the first functional component I made in React
 function App() {
   return (
     <>
+    <form>
+    <input placeholder="Enter your name"></input>
+    </form>
     </>
   );
 }
+
+
 
 export default App;
