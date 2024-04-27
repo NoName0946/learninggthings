6bfacf14bcae177a04c45930cf14142f38272591
NoName0946 <168062386+NoName0946@users.noreply.github.com>
2024-04-27 10:39:38 +0000

    This is the end of ch1 of learning

--- continued/myapp/src/App.js
+++ continued/myapp/src/App.js
@@ -1,3 +1,10 @@
+// THIS SECTION WAS ALL ABOUT RULES ABOUT GIVING CLASS NAMES AND IMPORTING COMPONENTS FROM OTHER FILES, HE ALSO TALKED ABOUT FRAGMENTS 
+
+
+
+
+
+
 /* //THis is the first functional component I made in React
 function App() {
   return (
@@ -7,15 +14,22 @@ function App() {
     </form>
     </>
   );
+}  */
+
+
+/* function App() {
+  return(<>
+  <img alt="This is supposed ot be an image"></img>.   ***{/*  Image needs to be definned like this otherwise it will give out errors because we are writing JSX and not HTML.}***
+  <h1 className="THis is how you give a class name in react,, we dont write class='' we use className=''">This is a heading 1</h1>
+  </>)
 } */
 
-
-function App() {
-  return(<>
-  <img></img> //Image needs to be definned like this otherwise it will give out errors because we are writing JSX and not HTML.
-  <h1 className = "THis is how you give a class name in react,, wedont write class='' we use className=''">This is a heading 1</h1>
-  </>)
+function App(){
+  return(
+    <>
+    
+    </>
+  )
 }
-
 
 export default App;
